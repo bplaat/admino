dcd890f6f9c4cbdc902c5d67a3ddb4984be4fe7c
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-05-21 11:04:33 +0200

    Add object add and remove buttons

--- src/main/java/ml/bastiaan/admino/App.java
+++ src/main/java/ml/bastiaan/admino/App.java
@@ -2,13 +2,20 @@
 
 package ml.bastiaan.admino;
 
+import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
-import javax.swing.SwingConstants;
+import javax.swing.ListSelectionModel;
 import javax.swing.JScrollPane;
 import javax.swing.UIManager;
 
@@ -34,8 +41,16 @@ public class App implements Runnable {
         frame.setSize(1280, 720);
         frame.setLocationRelativeTo(null);
 
+        JPanel root = new JPanel(new GridBagLayout());
+
+        JScrollPane rootScrollPane = new JScrollPane(root);
+        rootScrollPane.setBorder(null);
+        frame.add(rootScrollPane);
+
         JTabbedPane tabs = new JTabbedPane();
-        frame.add(tabs);
+        tabs.setPreferredSize(new Dimension(640, 480));
+        Insets insets = new Insets(4, 4, 4, 4);
+        root.add(tabs, new GridBagConstraints(0, 0, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, insets, 0, 0));
 
         // Student Tab
         List<Student> students = new ArrayList<Student>();
@@ -46,9 +61,32 @@ public class App implements Runnable {
         students.add(new Student("Michiel", "de Korte", Sex.MALE, "Technische Informatica", "TI1B"));
 
         StudentTableModel studentTableModel = new StudentTableModel(students);
+
+        JPanel studentTab = new JPanel(new BorderLayout());
+
         JTable studentTable = new JTable(studentTableModel);
+        studentTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         studentTable.setDefaultEditor(Sex.class, new SexCellEditor());
-        tabs.addTab("Students", new JScrollPane(studentTable));
+        studentTab.add(new JScrollPane(studentTable), BorderLayout.CENTER);
+
+        JPanel studentButtons = new JPanel();
+        studentTab.add(studentButtons, BorderLayout.PAGE_END);
+
+        JButton studentAddButton = new JButton("Add new student");
+        studentAddButton.addActionListener((ActionEvent event) -> {
+            students.add(new Student("?", "?", Sex.OTHER, "?", "?"));
+            studentTableModel.fireTableDataChanged();
+        });
+        studentButtons.add(studentAddButton);
+
+        JButton studentRemoveButton = new JButton("Remove selected student");
+        studentRemoveButton.addActionListener((ActionEvent event) -> {
+            students.remove(studentTable.getSelectedRow());
+            studentTableModel.fireTableDataChanged();
+        });
+        studentButtons.add(studentRemoveButton);
+
+        tabs.addTab("Students", studentTab);
 
         // Subject Tab
         List <Subject> subjects = new ArrayList<Subject>();
@@ -65,8 +103,35 @@ public class App implements Runnable {
         subjects.add(new Subject("Afsturen", 4));
 
         SubjectTableModel subjectTableModel = new SubjectTableModel(subjects);
+
+        JPanel subjectTab = new JPanel(new BorderLayout());
+
         JTable subjectTable = new JTable(subjectTableModel);
-        tabs.addTab("Subjects", new JScrollPane(subjectTable));
+        subjectTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        subjectTab.add(new JScrollPane(subjectTable), BorderLayout.CENTER);
+
+        JPanel subjectButtons = new JPanel();
+        subjectTab.add(subjectButtons, BorderLayout.PAGE_END);
+
+        JButton subjectAddButton = new JButton("Add new subject");
+        subjectAddButton.addActionListener((ActionEvent event) -> {
+            subjects.add(new Subject("?", 0));
+            subjectTableModel.fireTableDataChanged();
+        });
+        subjectButtons.add(subjectAddButton);
+
+        JButton subjectRemoveButton = new JButton("Remove selected subject");
+        subjectRemoveButton.addActionListener((ActionEvent event) -> {
+            subjects.remove(subjectTable.getSelectedRow());
+            subjectTableModel.fireTableDataChanged();
+        });
+        subjectButtons.add(subjectRemoveButton);
+
+        tabs.addTab("Subjects", subjectTab);
+
+        JPanel buttons = new JPanel();
+        buttons.setPreferredSize(new Dimension(320, 480));
+        root.add(buttons, new GridBagConstraints(2, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, insets, 0, 0));
 
         frame.setVisible(true);
     }
